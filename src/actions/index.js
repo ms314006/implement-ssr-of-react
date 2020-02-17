cc24af956ba47c63f064de1f73e28828bc306f45
GQSM <ms314006@hotmail.com>
2020-02-17 22:17:06 +0800

    [CHANGE] 在 fetchRequestQuery 中改成用解構從 res 中取出 data

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,10 +3,10 @@ import axios from 'axios';
 export const FETCH_REQUEST_QUERY = 'fetch_request_query';
 
 export const fetchRequestQuery = async (dispatch) => {
-  const res = await axios.get('http://httpbin.org/get');
+  const { data } = await axios.get('http://httpbin.org/get');
 
   dispatch({
     type: FETCH_REQUEST_QUERY,
-    payload: res.data,
+    payload: data,
   });
 };
