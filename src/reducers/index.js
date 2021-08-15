7f7dd79f032e74c26545eed99e52ad067c32d696
GQSM <ms314006@hotmail.com>
2021-08-15 11:37:46 +0800

    modify: 把 request 的 reducer 和 combine reducers 的部分拆成兩個檔案

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,15 +1,6 @@
 import { combineReducers } from 'redux';
-import { FETCH_REQUEST_QUERY } from '../actions';
-
-const requestReducer = (state = {}, action) => {
-  switch (action.type) {
-    case FETCH_REQUEST_QUERY:
-      return action.payload;
-    default:
-      return state;
-  }
-};
+import request from './request';
 
 export default combineReducers({
-  request: requestReducer,
+  request,
 });
